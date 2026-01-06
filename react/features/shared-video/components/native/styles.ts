1beb3c28c119c866891cd2d70741e67e590eadd6
lizz <1335206475@qq.com>
2026-01-06 16:32:16 +0800

    feat: 信号源样式

--- react/features/shared-video/components/native/styles.ts
+++ react/features/shared-video/components/native/styles.ts
@@ -7,5 +7,62 @@ export default {
         flex: 1,
         flexDirection: 'column',
         justifyContent: 'center'
+    },
+    textContainer: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        textAlign: 'center'
+    },
+    extendedVideoContainer: {
+        alignItems: 'center',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%'
+
+        // borderWidth: 2,
+        // borderColor: '#FFFFFF',
+        // borderStyle: 'solid'
+    },
+    multipleVideoContainer: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        flexWrap: 'wrap'
+    },
+    videoWrapper: {
+        boxSizing: 'border-box',
+        color: '#FFFFFF',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+
+        // borderWidth: 1,
+        // borderColor: '#FFFFFF',
+        // borderStyle: 'solid'
+    },
+    largeTwoVideoLeftContainer: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        flexWrap: 'wrap'
+
+        // borderWidth: 1,
+        // borderColor: '#FFFFFF',
+        // borderStyle: 'solid'
+    },
+    largeTwoVideoRightContainer: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        flexWrap: 'wrap'
+
+        // borderWidth: 1,
+        // borderColor: '#FFFFFF',
+        // borderStyle: 'solid'
     }
 };
