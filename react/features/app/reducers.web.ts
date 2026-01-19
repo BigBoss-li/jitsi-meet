4b097f3bd76cacd7dab8434b653757fce4508e1f
lizz <1335206475@qq.com>
2026-01-19 11:35:54 +0800

    feat: add meeting file redux

--- react/features/app/reducers.web.ts
+++ react/features/app/reducers.web.ts
@@ -18,5 +18,6 @@ import '../talk-while-muted/reducer';
 import '../virtual-background/reducer';
 import '../web-hid/reducer';
 import '../meeting-signal/reducer';
+import '../meeting-file/reducer';
 
 import './reducers.any';
